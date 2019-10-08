6b4a4331387cf8f1313dfca031761d748023f7f1
oppressionslayer <37212196+oppressionslayer@users.noreply.github.com>
2019-10-08 14:55:14 -0600

    Update devilsnum.py

--- devilsnum.py
+++ devilsnum.py
@@ -12,8 +12,8 @@ def Xploderiter(s, iter=1):
   return s
 
 def createdevilsnumSHIFT(hm):
-  if hm % 2==0:
-    return "This works with odd numbers only right now"
+  #if hm % 2==0:
+  #  return "This works with odd numbers only right now"
   y=1 
   j=hm
   loopbreak = j + 1
@@ -32,6 +32,9 @@ def creategodsanswerfromdevilsnum(j, y=0, yd=1):
   firstrun=True
   iterx=0
   origj=j
+  even=False
+  if j % 2 == 0:
+     even=True
   j = createdevilsnumSHIFT(j)
   if j == "This works with odd numbers only right now":
     return "This works with odd numbers only right now"
@@ -42,11 +45,77 @@ def creategodsanswerfromdevilsnum(j, y=0, yd=1):
     print("yd=the number of times you double y per iterations")
     print("Devils Number: The number generated to get all the results you see. Your number is not used ")
     print(" instead, J=Devils Number, which is then used to calculate all results, leading to your number")
-    print("Remember, all results are calculated with the devils number, and it gets to your number with ")
-    print("different most values you use as Y, not just powers of 2!")
     print("")
     print("Many times the 3rd column will always give you your original number for different y and yd\'s")
-    print("This doesn't work everytime, but you'll notice it works a lot, your number will be in 3rd column")
+    print("This doesn't work everytime, but you'll notice it works a lot, your numner will be in 3rd column")
+    print("")
+    print("J = {} - The Devils number for all operations below".format(j))
+    print("1st Column: iterx")
+    print("2nd Column: y=Xploderiter(y,yd) # Meaning you could div//2 yd times to the previous answer")
+    print("3rd Column: j-(((y*2)+1)^j)")
+    print("4th Column: (((abs(j-(y*2^j)))^((y*2)+1))+2)//2")
+    print("5th Column: j-(((y*2)+1)^j)^previous(j-(((y*2)+1)^j))")
+    print("")
+    print("Your Number: {}".format(origj))
+    print("Your Numbers Xploderiter: {}".format(origj*2+1))
+    if even == False:
+      print("Your Devils Number and it's advocate: {}, {}".format(j, j+1))
+    else:
+      print("Your Devils Number and it's advocate: {}, {}".format(j, j-1))
+    print("XOR Columns 2 and 3 to see your numbers Xploderiter") 
+    print("")
+    if even == False:
+       print(iterx, (y*2)+1,doublin, (((abs(j-(y*2^j)))^((y*2)+1))+2)//2, doublin^prevdoublin, )  
+    else:
+       print(iterx, (y*2)+1,doublin, (((abs(j-(y*2^j)))^((y*2)+1)))//2, doublin^prevdoublin, ) 
+    iterx+=1
+    while y<j+1: 
+       prevdoublin = doublin
+       for x in range(0,yd):
+         y=(y<<1)+1 
+       doublin = j-(((y*2)+1)^j)
+       if even == False:
+          print(iterx, (y*2)+1,doublin, (((abs(j-(y*2^j)))^((y*2)+1))+2)//2, doublin^prevdoublin, )  
+       else:
+          print(iterx, (y*2)+1,doublin, (((abs(j-(y*2^j)))^((y*2)+1)))//2, doublin^prevdoublin, )  
+       iterx+=1
+    godsnum=Xploderiter(y)^j
+    godsanswer=abs(j-(((y*2)+1)^j))
+  print("")
+  print("Your Number: {}".format(origj))
+  print("Your Numbers Xploderiter: {}".format(origj*2+1))
+  if even == False:
+    print("Your Devils Number and it's advocate: {}, {}".format(j, j+1))
+  else:
+    print("Your Devils Number and it's advocate: {}, {}".format(j, j-1))
+  print("XOR Columns 2 and 3 to see your numbers Xploderiter")
+  print("")
+  #return len(bin(j)[2:]), j, origj 
+  print("IF your on repl.it, just copy paste the entry below this message into the window and hit enter/return on your keboard keep testing different numbers:")
+  print("creategodsanswerfromdevilsnum(1009,187)")
+
+
+def creategodsanswerfromdevilsnum2(j, y=0, yd=1):
+  doublin=0
+  prevdoublin=0
+  godsnum=0
+  godsanswer=0
+  firstrun=True
+  iterx=0
+  origj=j
+  j = createdevilsnumSHIFT(j)
+  if j == "This works with odd numbers only right now":
+    return "This works with odd numbers only right now"
+  for x in range(0,1): 
+    print("")
+    print("USAGE: creategodsanswerfromdevilsnum(yournumber, y, yd)")
+    print("y=the number you double by")
+    print("yd=the number of times you double y per iterations")
+    print("Devils Number: The number generated to get all the results you see. Your number is not used ")
+    print(" instead, J=Devils Number, which is then used to calculate all results, leading to your number")
+    print("")
+    print("Many times the 3rd column will always give you your original number for different y and yd\'s")
+    print("This doesn't work everytime, but you'll notice it works a lot, your numner will be in 3rd column")
     print("")
     print("1st Column: iterx")
     print("2nd Column: y=Xploderiter(y,yd) # Meaning you could div//2 yd times to the previous answer")
@@ -55,25 +124,25 @@ def creategodsanswerfromdevilsnum(j, y=0, yd=1):
     print("5th Column: j-(((y*2)+1)^j)^previous(j-(((y*2)+1)^j))")
     print("")
     print("Your Number: {}".format(origj))
-    print("Your Numbers Xploderiter: {} or {}".format(origj*2, origj*2+1))
+    print("Your Numbers Xploderiter: {}".format(origj*2+1))
     print("Your Devils Numbers: {}, {}".format(j, j+1))
-    print("XOR Columns 2 and 3 to see your numbers Xploderiter")
     print("")
-    print(iterx, (y*2)+1,doublin, (((abs(j-(y*2^j)))^((y*2)+1))+2)//2, doublin^prevdoublin, )  
+    print(iterx, (y*2)+1,doublin, (((abs(j-(y*2^j)))^((y*2)))+2)//2, doublin^prevdoublin, )  
     iterx+=1
     while y<j+1: 
        prevdoublin = doublin
        for x in range(0,yd):
-         y=(y<<1)+1 
-       doublin = j-(((y*2)+1)^j)
-       print(iterx, (y*2)+1,doublin, (((abs(j-(y*2^j)))^((y*2)+1))+2)//2, doublin^prevdoublin, )  
+         y=y*2
+       doublin = j-(((y*2))^j)
+       print(iterx, (y*2),doublin, (((abs(j-(y*2^j)))^((y*2)))+2)//2, doublin^prevdoublin, )  
        iterx+=1
     godsnum=Xploderiter(y)^j
-    godsanswer=abs(j-(((y*2)+1)^j))
+    godsanswer=abs(j-(((y*2))^j))
   print("")
   print("Your Number: {}".format(origj))
-  print("Your Numbers Xploderiter: {} or {}".format(origj*2, origj*2+1))
+  print("Your Numbers Xploderiter: {}".format(origj*2+1))
   print("Your Devils Numbers: {}, {}".format(j, j+1))
-  print("XOR Columns 2 and 3 to see your numbers Xploderiter")
   print("")
   #return len(bin(j)[2:]), j, origj 
+
+creategodsanswerfromdevilsnum(1009)
