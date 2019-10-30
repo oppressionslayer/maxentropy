889d49a77dc276a7225c90960ab558cc60b562c7
oppressionslayer <37212196+oppressionslayer@users.noreply.github.com>
2019-10-30 14:34:59 -0600

    Update DecompressNumberWithHuffnan.py

--- DecompressNumberWithHuffnan.py
+++ DecompressNumberWithHuffnan.py
@@ -1,51 +1,48 @@
-# Decompresses the number 1009732533765201 using Huffman
+# Uncompresses the number: 1009732533765201
+# which has the binary:
+# 0b11100101100101100010101100111111100100010001010001
+# Our compressed binary is:
+# 110011111000011010110011
+# We compress a variable length pattern that is decoded 
+# to try to acheive nearly as good or better compression 
+# than binary compression
+# S is a representation of XOR math that is 
+# required to rebuild the original number
+# so we don't compress the number itself, we compress
+# the math to rebuild the number instead
+
+from functools import reduce
 
 s={'11': '++--', '01': '-+-+-', '000': '+-++++++', '001': '--', '100': '--+-', '101': '----++-+--'}
-
+compressed_binary='110011111000011010110011'
 
 def decodepattern(pattern, j=1):
    x=2
    for c in range(len(pattern)):
-     if pattern[c]=='+':
+     if pattern[c]=='+' or pattern[c]=='1':
        j^=x
      else:
        j^=-x
      x<<=1
    return abs(j)
 
-match = True
-stream = 0
-match = False
-peek=''
-peekstart=0
-peekend=1
-sptrint=0b110011111000011010110011
-sptrbuff=''
-sptr = sptrbuff + bin(sptrint)[2:]
-sptrlen = len(sptr)
-bytestr=b''
-while peekstart+peekend <= sptrlen:
-  match=False
-  while match == False:
-    peek = sptr[peekstart:peekstart+peekend] 
-    findptr = s.get(peek)
-    while findptr == None:
-       peek = sptr[peekstart:peekstart+peekend+stream]
-       stream=stream+1
-       findptr = s.get(peek)
-    if stream == 0:
-      peekstart=peekstart+peekend+stream
-    else:
-      peekstart=peekstart+peekend+stream-1
-    stream=0
-    bytestr+=s.get(peek).encode()
-    match = True
+def lefttoright(aggregate, bit):
+    prefix, out = aggregate
+    key = prefix + bit
+    return ('', out + s[key]) if (key in s) else (key, out)
+
+remainder,uncompressed_string = reduce(lefttoright, compressed_binary, ('', ''))
+print(uncompressed_string)
+
+answer = decodepattern(uncompressed_string)
+
 print("Bin length of 1009732533765201 is 50 which is the same as the pattern length")
 print("Original Bin of 1009732533765201 is: ")
 print(bin(1009732533765201)[2:])
 print("Original Pattern:")
-print(bytestr)
+print(uncompressed_string)
 print("Compressed to: ", bin(13600435)[2:])
-print("110011111000011010110011 uncompressed to: ")
-print("")
-print(decodepattern(bytestr.decode()))
+print("110011111000011010110011 uncompressed to the original string pattern of : ")
+print(uncompressed_string)
+print("Which then i was able to recreate the original integer of: ")
+print(decodepattern(uncompressed_string))
